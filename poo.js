944b7fc374f04dd39de7152272383138ba73446c
Andrekrykhtine <andkryk@gmail.com>
2026-01-13 16:12:35 -0300

    Formata o código das classes Pessoa e carro para melhorar a legibilidade

--- poo.js
+++ poo.js
@@ -1,27 +1,23 @@
-
-class Pessoa{
-    constructor(nome, idade){
-        this.nome=nome;
-        this.idade=idade;
-    }
-    falar(){
-        console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
-    }
+class Pessoa {
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+  falar() {
+    console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+  }
 }
-const pessoa1=new Pessoa("Ana", 25);
+const pessoa1 = new Pessoa("Ana", 25);
 pessoa1.falar();
 
-class carro{
-    constructor(marca,ano)
-    {
-        this.marca=marca;
-        this.ano=ano;
-    }
-dirigir(){
-    console.log(`O carro da marca ${this.marca} do ano ${this.ano} está dirigindo.`);
+class carro {
+  constructor(marca, ano) {
+    this.marca = marca;
+    this.ano = ano;
+  }
+  dirigir() {
+    console.log(
+      `O carro da marca ${this.marca} do ano ${this.ano} está dirigindo.`
+    );
+  }
 }
-}
-//teste 10666
-
-
-
